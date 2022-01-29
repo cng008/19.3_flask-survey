40c0bbbae43797cb3f2d3dbb5d9f914d3618af53
Christien Ng <ngchristien@gmail.com>
2022-01-29 14:24:12 -0800

    survey ui

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'shhhh'
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+responses = []
+
+
+@app.route('/')
+def index():
+    """ start survey page showing title, instructions, start button """
+
+    return render_template("start.html", survey=survey)
+
+
+@app.route("/begin", methods=["POST"])
+def start_survey():
+    """ go to first question """
+
+    responses = []
+    
+    return redirect("/questions/0")
+
+
+@app.route('/questions/<int:q_id>')
+def quest(q_id):
+    """ show 1 question per page """
+
+    if responses is None: 
+        # trying to access question page too soon
+        return redirect('/')
+
+    if len(responses) == len(survey.questions):
+        # They've answered all the questions! Thank them.
+        return redirect("/thanks")
+
+    if len(responses) != q_id :
+        # Trying to access questions out of order.
+        flash(f"Error {q_id}: You can't go there yet.")
+        return redirect(f"/questions/{len(responses)}")
+
+    question = survey.questions[q_id]
+    return render_template("questions.html", question_num=q_id, question=question)
+
+
+@app.route('/answer', methods=["POST"])
+def ans():
+    """ Save response and redirect to next question """
+
+    # get the response choice
+    choice = request.form['answer']
+
+    responses.append(choice)
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/thanks")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+
+@app.route("/thanks")
+def complete():
+    """ Survey complete. Show thanks page """
+
+    return render_template("thanks.html")
